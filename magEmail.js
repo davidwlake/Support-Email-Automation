abeee145d244372c1d9d4a0c5be2a4de64bef6f7
David Lake <davidwlake@gmail.com>
2017-01-10 17:40:12 -0500

    Fixing Email Template
    Remove duplcate line.

--- magEmail.js
+++ magEmail.js
@@ -33,7 +33,7 @@ function writeEmail() {
         }
     }
     
-    var confirmation = "Hello " + localStorage.getItem("firstName") + ", \n\nThank you for contacting Dealer.com Dealer Support.\n\nWe have received your email and created support case: " + caseNumber + ".\nFor case status updates please respond directly to this email. For immediate assistance please contact us at 888-694-3032.\n\nFor case status updates please respond directly to this email.\n\nThank you for your continued partnership,\n\n Dealer.com Support";
+    var confirmation = "Hello " + localStorage.getItem("firstName") + ", \n\nThank you for contacting Dealer.com Dealer Support.\n\nWe have received your email and created support case: " + caseNumber + ".\nFor case status updates please respond directly to this email. For immediate assistance please contact us at 888-694-3032.\n\nThank you for your continued partnership,\n\n Dealer.com Support";
     document.getElementById('p6').value = "Re: " + title + " (Case #" + caseNumber + ")";
     document.getElementById('p7').value = confirmation;
 
